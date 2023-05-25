34f51b433137a64c35c7ccfca72cbc5f2b826a3a
Xokhezi <nemec.nuovo@gmail.com>
2023-05-25 19:51:55 +0200

    Card

--- Tager/src/App.tsx
+++ Tager/src/App.tsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import Tager from "./Components/Tager";
+import { Box } from "@mui/joy";
 function App() {
   const [tags, setTags] = useState(["React", "Angular", "Vue", "CSS"]);
 
   return (
     <>
-      <Tager setTags={setTags} tags={tags} />
+      <Box
+        sx={{
+          borderRadius: 15,
+          padding: 5,
+          height: 400,
+          width: 400,
+          background: "white",
+        }}
+      >
+        <h2>Tager</h2>
+        <Tager setTags={setTags} tags={tags} />
+      </Box>
     </>
   );
 }
